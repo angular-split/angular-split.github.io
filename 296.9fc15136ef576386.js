51a76042fb316d99b46d3bc38ae345663d89e1fb
Bram Borggreve <beeman@beeman.nl>
2023-11-15 14:02:20 +0000

    Auto-generated commit [ci skip]

--- 296.9fc15136ef576386.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkangular_split_app=self.webpackChunkangular_split_app||[]).push([[296],{9296:(_,s,a)=>{a.r(s),a.d(s,{ExamplesModule:()=>e});var o=a(7276),E=a(1020),u=a(4769);let e=(()=>{var l;class n{}return(l=n).\u0275fac=function(t){return new(t||l)},l.\u0275mod=u.oAB({type:l}),l.\u0275inj=u.cJS({imports:[o.Bz.forChild([...E.y])]}),n})()}}]);
