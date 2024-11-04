1c2780f2c90080b7f3fe8f1e767c76893435d7ea
Jeffrey Bosch <jefiozie.bosch@gmail.com>
2024-11-04 14:58:58 +0100

    Auto-generated commit

---
+++ chunk-K6ZBELTV.js
@@ -0,0 +1,1 @@
+import{b as r}from"./chunk-SJDOEHTM.js";import{G as o,M as e,Ob as i}from"./chunk-J6TELLLA.js";var n=(()=>{class t{static{this.\u0275fac=function(m){return new(m||t)}}static{this.\u0275mod=e({type:t})}static{this.\u0275inj=o({imports:[i.forChild([...r])]})}}return t})();export{n as ExamplesModule};
