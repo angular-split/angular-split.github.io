3b58cbc885ed6d5695a50d91805effeafe7469cb
Jeffrey Bosch <jefiozie.bosch@gmail.com>
2025-01-11 09:47:27 +0100

    Auto-generated commit

--- chunk-SF3IOWHW.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./chunk-ZLZGFJ2P.js";import{G as o,M as e,Ob as i}from"./chunk-J6TELLLA.js";var n=(()=>{class t{static{this.\u0275fac=function(m){return new(m||t)}}static{this.\u0275mod=e({type:t})}static{this.\u0275inj=o({imports:[i.forChild([...r])]})}}return t})();export{n as ExamplesModule};
